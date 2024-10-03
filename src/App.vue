14f49c759d8752e05f6c168e29bf830cc3b79db2
Van Nguyen Thi Khanh <VanNTK.B22CN892@stu.ptit.edu.vn>
2024-10-03 18:26:26 +0700

    100%

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<!-- <script setup>
+  import {ref} from "vue";
+
+  const count = ref(0);
+</script> -->
+  
+
+<script>
+  export default{
+    data(){
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      Add(){
+        this.count = this.count + 1
+      }, 
+      Sub(){
+        this.count = this.count - 1
+      }
+    }
+    
+  }
+</script>
+
+
+<template>
+  <main>
+    <div>
+      <h4>
+        The current count is ...
+      </h4>
+      <h1>{{ count }}</h1>
+      <button @click="Sub()">-</button>
+      <button @click="Add()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main{
+    background-color:  aliceblue;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items:  center;
+    justify-content:  center;
+  }
+  div{
+    text-align: center;
+  }
+</style>
